e1761225d4b7d8c1e1baf57f4b6f82a9296c861e
Md Monerujamman Munna <mdmonerujammanmunna@gmail.com>
2026-05-01 20:09:47 +0600

    add Sign Up from

--- src/app/(other)/Login/page.jsx
+++ src/app/(other)/Login/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button, Description, FieldError, Form, Input, Label, TextField } from "@heroui/react";
+import Link from "next/link";
 const LoginPage = () => {
     const onSubmit = (e) => {
         e.preventDefault();
@@ -13,43 +14,6 @@ const LoginPage = () => {
         alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
     };
     return (
-        //       
-
-
-        //         <form className="space-y-4">
-        //             <div>
-        //                 <label className="text-sm font-medium text-gray-700">
-        //                     Email
-        //                 </label>
-        //                 <input
-        //                     type="email"
-        //                     placeholder="Enter your email"
-        //                     className=""
-        //                 />
-        //             </div>
-
-        //             <div>
-        //                 <label className="text-sm font-medium text-gray-700">
-        //                     Password
-        //                 </label>
-        //                 <input
-        //                     type="password"
-        //                     placeholder="Enter your password"
-        //                     className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 outline-none"
-        //                 />
-        //             </div>
-
-        //             <button
-        //                 type="button"
-        //                 className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
-        //             >
-        //                 Login
-        //             </button>
-        //         </form>
-
-        //     </div>
-        // </div>
-
         <div className="min-h-screen flex items-center justify-center bg-[var(--main-color)] text-white px-4">
             <Form className="flex  flex-col gap-4 bg-white w-full max-w-md p-8 rounded-2xl shadow-lg" onSubmit={onSubmit}>
                 <h2 className="text-3xl font-bold text-center text-gray-800">
@@ -98,15 +62,15 @@ const LoginPage = () => {
                 </TextField>
                 <div className="flex gap-2">
                     <Button type="submit" className="w-full">
-                        Submit
+                        Log in
                     </Button>
                 </div>
 
                 <p className="text-center text-sm text-gray-500 ">
                     Don’t have an account?{" "}
-                    <a href="#" className="text-blue-500 hover:underline">
+                    <Link href="/SignUP" className="text-blue-500 font-bold hover:underline">
                         Sign up
-                    </a>
+                    </Link>
                 </p>
             </Form>
         </div>
